ead1f9ea395cde186825b38caa57bc188a737493
DAUL0105 <gooddaul@daum.net>
2016-11-22 09:03:13 +0900

    Create Header.java

---
+++ src/Header.java
@@ -0,0 +1,11 @@
+public class Header extends Node {
+	public int size = 1; // number of '#'
+    Header(int n){
+		size=n;
+		}
+
+	@Override
+    public void accept(MDElementVisitor v) {
+        v.visit(this);
+    }
+}
