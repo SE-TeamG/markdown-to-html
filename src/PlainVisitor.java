30a42a97e567d7091bbac72d4b8d0fee6a89f0ef
tnqls0332 <o_osubin@naver.com>
2016-11-23 19:20:19 +0900

    Update PlainVisitor.java

--- src/PlainVisitor.java
+++ src/PlainVisitor.java
@@ -4,25 +4,27 @@ public class PlainVisitor implements MDElementVisitor {
     private String currentdir = "";
     public void visit(Document document) {
 		//get all node from nlist in document and make it accept()
-System.out.print("\n\n\n TRANSLATED->\n");
 		document.nlist.get(0).accept(new PlainVisitor()); // the way to visit 'only' first node (it is example. you have to revise)
 
     }
 
 
     public void visit(Header h){
-		//print <headline>. you have to revise it to print on new file.
-		System.out.print("<h1>"); 
-	    
-		//token visit
-		System.out.print((String)h.tlist.get(0).tString); //you have to revise it to visit 'all' tokens.
-		//print </headline>
-		System.out.print("</h1>");
-	}
-	public void visit(Node n){
-		System.out.println("why??");
-
-		}
+	try
+         {
+             FileWriter fw = new FileWriter("ex2.html");
+             BufferedWriter bw = new BufferedWriter(fw);
+             bw.write("<h1>");
+             bw.write((String)h.tlist.get(0).tString);
+             bw.write("</h1>");
+             
+             bw.close();
+         }
+         catch (IOException e)
+         {
+             System.err.println(e);
+             System.exit(1);
+         }
 	
 	public void visit(QuotedBlock qb){}
 }
