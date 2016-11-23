ee7969610e3d21fb8cd4c18f60ce6b4a59ebdbc3
DAUL0105 <gooddaul@daum.net>
2016-11-23 10:05:40 +0900

    Update PlainVisitor.java

--- src/PlainVisitor.java
+++ src/PlainVisitor.java
@@ -3,19 +3,20 @@ import java.util.Iterator;
 public class PlainVisitor implements MDElementVisitor {
     private String currentdir = "";
     public void visit(Document document) {
-		//document list 내에 nlist에서 node꺼내서 visit -> while문과 .accept() 가 사용됨.
+		//get all node from nlist in document and make it accept()
 System.out.print("\n\n\n TRANSLATED->\n");
-		document.nlist.get(0).accept(new PlainVisitor()); // 첫번째 노트를 visit 하는 방법(예제)
+		document.nlist.get(0).accept(new PlainVisitor()); // the way to visit 'only' first node (it is example. you have to revise)
 
     }
 
 
     public void visit(Header h){
-		//outputfile에 <headline> 출력
-		System.out.print("<h1>"); // file에 써야 하지만 임의로 프린트 하도록 해둠.
-		//token에 'visit'하여 내용물 출력
-		System.out.print((String)h.tlist.get(0).tString); // 지금은 하나만 출력했지만 사실 티리스트들 전부 '양식에 맞추어 출력해야함.
-		//outputfile에 </headline> 출력
+		//print <headline>. you have to revise it to print on new file.
+		System.out.print("<h1>"); 
+	    
+		//token visit
+		System.out.print((String)h.tlist.get(0).tString); //you have to revise it to visit 'all' tokens.
+		//print </headline>
 		System.out.print("</h1>");
 	}
 	public void visit(Node n){
