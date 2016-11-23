e0eb541df9af44d6ec10b98e85e3ce31ea97a8ee
DAUL0105 <gooddaul@daum.net>
2016-11-23 09:26:46 +0900

    Update MDElementVisitor.java

--- src/MDElementVisitor.java
+++ src/MDElementVisitor.java
@@ -1,4 +1,5 @@
 public interface MDElementVisitor {
     public abstract void visit(Document document);
     public abstract void visit(Header h);
+    public abstract void visit(QuotedBlock qb);
 }
