8d66ea026ccab87c657230fd23af4cfe6b66eb4c
tldhs <tldhstla@gmail.com>
2016-11-24 21:41:44 +0900

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,33 +1,53 @@
+package helloworld;
+
+
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
-public class Node implements MDElement {
-	public String total="";
-    public List<Token> tlist = new ArrayList<Token>();
+public class Node implements MDElement  {
+	
+public String total="";
+public List<Token> tlist = new ArrayList<Token>();
+public List<Node> nlist = new ArrayList<Node>();
+public Node(String tString){tString=total;}
 
-    public void accept(MDElementVisitor v){};
-    public Node create(String s){
+	public Node create(String s){
+
+			int pos;
+			String s1="";
 			total=s;
+	
 			if(s.compareTo("")==0||s.compareTo(" ")==0||s.compareTo("\n")==0)
 				return null;
 			else if(s!=null&&s.charAt(0)=='#'){
-				Header h = new Header(1);
-				/*while( ){ // 
-				you have to parse total to several tokens and add them to tlist.
-				the way it work is similar with the way add node in nlist which is excuted in node MDParser class.
-				you can refer to MDParser class.
-				}*/
-				h.tlist.add(new Token(s.substring(1))); // the way to insert just 'one' token. you have to revise it to insert all tokens.
+				int i=0;
+				while(s.charAt(i)=='#')
+				{i++;}
 
-				return h;
+					Header h = new Header(i-1);
+					h.tlist.add(new Token(s.substring(1))); // the way to insert just 'one' token. you have to revise it to insert all tokens.
+
+					return h;//-----?
 				}
-			else{
-
-				return new QuotedBlock(); // you have to revise it like above.
-
+			else if(s!=null&&s.charAt(0)=='>')
+			{
+				return new QuotedBlock();
 			}
+return null;
 		}
 
 
+//@Override
+public void accept(MDElementVisitor v) {
+	// TODO Auto-generated method stub
+	
+}
 
 }
+
+
+
+
+
+
